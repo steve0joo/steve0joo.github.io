3b857f9999b7e81634bf8b8f911b28b7aa265b51
Steve-Joo1 <steve_joo@icloud.com>
2025-02-25 00:49:09 -0500

    Modified docusaurus.config.js for deployment error

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,16 +15,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://github.com',
+  url: 'https://steve0joo.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/steve0joo.github.io/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'steve0joo', // Usually your GitHub org/user name.
   projectName: 'steve0joo.github.io', // Usually your repo name.
   deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
